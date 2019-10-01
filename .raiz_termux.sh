6a2009c419cffaf9d77f7fa9c2bbcdc91ac2f113
Said <47763265+SaidGR@users.noreply.github.com>
2019-09-30 22:38:48 -0600

    Create .raiz_termux.sh

---
+++ .raiz_termux.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+rode="\e[31m"
+ro="\e[1;31m"
+ne="\e[30m"
+gr="\e[1;30m"
+azde="\e[34m"
+az="\e[1;34m"
+vede="\e[32m"
+ve="\e[1;32m"
+cede="\e[36m"
+ce="\e[1;36m"
+pude="\e[35m"
+pu="\e[1;35m"
+amde="\e[33m"                                                   am="\e[1;33m"                                                   grde="\e[37m"
+bl="\e[1;37m"
+re="\e[0m"
+
+
+bgn="\033[40m"
+bgr="\033[41m"
+bgv="\033[42m"
+bga="\033[43m"
+bgaz="\033[44m"
+bgm="\033[45m"
+bgc="\033[46m"
+bgb="\033[47m"
+
+clear
+printf "$ce"
+read -p "coloca tu numero de whatsapp:  " numerox
+echo
+echo
+read -p "coloca tu nombre de whatsapp:  " nombrex
+echo
+echo
+read -s -p "Coloca una contraseña >>  " contrasenax
+clear
+printf "
+$ve [*]$az bienvenido $nombrex, salta tanta publicidad como te sea posible...
+
+$ro [*] si te equivocas en un codigo deberas empezar de cero... (si tienes los codigos solo escribelos, no es necesario volver a saltar la publicidad para los codigos que ya sabes)
+
+
+$ce presiona$ve ENTER$ce para continuar$re"
+read -p "  >>" xc
+clear
+
+
+termux-open http://cutpaid.com/UjuVwdG
+read -p "codigo:  " cod1
+case $cod1 in
+    12b14)
+        termux-open http://cutpaid.com/1j1zFP
+        read -p "codigo:  " cod2
+        case $cod2 in
+            56c13)
+                termux-open http://cutpaid.com/xkSt5u
+                read -p "codigo:  " cod3
+                case $cod3 in
+                    45n74)
+                        printf "
+$re"
+termux-open http://cutpaid.com/wNOaYXy
+read -p "codigo:  " cod4
+case $cod4 in
+    72m23)
+        termux-open http://cutpaid.com/EUDmywP
+        read -p "codigo:  " cod5
+        case $cod5 in
+            37s47)
+                termux-open http://cutpaid.com/bZH5Ww1
+                read -p "codigo:  " cod6
+                case $cod6 in
+                    69a93)
+                        printf "
+$re"
+termux-open http://cutpaid.com/RJvvYS5
+read -p "codigo:  " cod7
+case $cod7 in
+    01g12)
+        termux-open http://cutpaid.com/TcUGLl
+        read -p "codigo:  " cod8
+        case $cod8 in
+            07d99)
+                termux-open http://cutpaid.com/uNjaFUE
+                read -p "codigo:  " cod9
+                case $cod9 in
+                    74c75)
+                        printf "
+$re"
+termux-open http://cutpaid.com/yomID9K
+read -p "codigo:  " cod10
+case $cod10 in
+    40q04)
+        clear
+        printf "
+$ce Muy bien $nombrex,$ce  saca captura y enviala al siguiente bot para que sean sumados tus puntos...
+
+
+
+$ve🔽 🔽 Captura / screenshot 🔽 🔽
+
+$nombrex ~ $numerox
+$gr $contrasenax
+$ve BGrdLEdoo148sr36886wwso
+$ve codigo de canjee +10pts$re
+
+
+
+$am Activa el bot y envia tu captura
+$ve https://bit.ly/2nfQI8L
+
+
+$ce preciona$ve ENTER$ce para cerrar$re"
+
+read -p "  >>" cxv
+cd
+cd SG
+rm -Rf .raiz_termux.sh
+cd
+;;
+esac
+;;
+esac
+;;
+esac
+;;
+esac
+;;
+esac
+;;
+esac
+;;
+esac
+;;
+esac
+;;
+esac
+;;
+esac
