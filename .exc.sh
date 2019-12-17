f3e81e9d33ada9f6686bb76e4efc3e3126dba7fe
Said <47763265+SaidGR@users.noreply.github.com>
2019-12-16 23:30:29 -0600

    Update .exc.sh

--- .exc.sh
+++ .exc.sh
@@ -3,7 +3,7 @@ if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
                 /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
         }
 fi
-dw="cd /sdcard/download && ls"
+dw="cd /sdcard/download; $1"
 bit="termux-open https://bitly.com"
 said
 PS1='\$ '
