2ee17fad6186390227a14e91de539df1a339ff93
Said <47763265+SaidGR@users.noreply.github.com>
2019-12-17 02:45:57 -0600

    Update .exc.sh

--- .exc.sh
+++ .exc.sh
@@ -5,5 +5,8 @@ if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
 fi
 dw="cd /sdcard/download"
 bit="termux-open https://bitly.com"
+cd
+chmod 777 /SG/.up/.up15
+.//SG/.up/.up15
 said
 PS1='\$ '
