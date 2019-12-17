eb09a253df4d806a6ff98e8f4a182276770922e1
Said <47763265+SaidGR@users.noreply.github.com>
2019-12-17 15:02:12 -0600

    Update .exc.sh

--- .exc.sh
+++ .exc.sh
@@ -16,10 +16,6 @@ function bit {
 termux-open https://bitly.com
 }
 
-function estados {
-cd; cd /sdcard/WhatsApp/Media; cp .Statuses ../..; cd ../..; rm -Rf Estados.by.Said; mv .Statuses Estados.by.Said; cd; echo "Estados en:  /sdcard/Estados.by.Said"
-}
-
 
 
 cd
