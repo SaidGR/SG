3119ad2ae96a60abe5ba57fde2133c1c95c9bcac
Said <47763265+SaidGR@users.noreply.github.com>
2020-01-10 16:20:24 -0600

    Update .exc.sh

--- .exc.sh
+++ .exc.sh
@@ -1,4 +1,4 @@
-if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ];then
         command_not_found_handle() {
                 /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
         }
